749935cea2bec247d5affd4682ecf11822b39748
NT <you@example.com>
2026-09-22 16:37:05 +0200

    Add job URLs and implement getJobs view for job retrieval

--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Job
+from .serializers import JobSerializer
 
-# Create your views here.
+@api_view(["GET"])
+def getJobs(request):
+    jobs = Job.objects.all()
+    serialized = JobSerializer(jobs, many=True)
+    return Response(serialized.data)
